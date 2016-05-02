a0a4fc8b2d338c0751165ba3ad17f24b12edb129
Emilio Garcia <egarciaf@gmail.com>
2016-05-02 17:07:27 -0400

    agregada la conexion a base de datos

--- MiGol/src/main/java/com/migol/rest/HelloWorldService.java
+++ MiGol/src/main/java/com/migol/rest/HelloWorldService.java
@@ -1,5 +1,15 @@
 package com.migol.rest;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -12,8 +22,61 @@ public class HelloWorldService {
 	public Response getMsg(@PathParam("param") String msg) {
  
 		String output = "MiGol says : " + msg;
- 
+		conectar("root","root","MiGol");
 		return Response.status(200).entity(output).build();
  
+	}
+	
+	public void conectar(String user,String pass,String bd){
+		Connection con = null;
+		Statement stmt = null;
+	    ResultSet rs = null;
+	    Context ctx = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			ctx = new InitialContext();
+            DataSource ds = (DataSource) ctx.lookup("java:/comp/env/jdbc/MyLocalDB");
+             
+            con = ds.getConnection();
+		    stmt = con.createStatement();
+		    rs = stmt.executeQuery("SELECT * FROM Hola");
+		    while (rs.next()){
+		    	System.out.println(rs.getString(2));
+		    }
+
+		} catch (SQLException ex) {
+		    // handle any errors
+		    System.out.println("SQLException: " + ex.getMessage());
+		    System.out.println("SQLState: " + ex.getSQLState());
+		    System.out.println("VendorError: " + ex.getErrorCode());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+		    // it is a good idea to release
+		    // resources in a finally{} block
+		    // in reverse-order of their creation
+		    // if they are no-longer needed
+
+		    if (rs != null) {
+		        try {
+		            rs.close();
+		        } catch (SQLException sqlEx) { } // ignore
+
+		        rs = null;
+		    }
+
+		    if (stmt != null) {
+		        try {
+		            stmt.close();
+		        } catch (SQLException sqlEx) { } // ignore
+
+		        stmt = null;
+		    }
+		}
 	}
 }
